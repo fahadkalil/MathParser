ac17d445a65b2988004f8b06ff9a1f7415c51e8b
Fahad Kalil <4945023+fahadkalil@users.noreply.github.com>
2025-11-04 15:10:18 -0300

    Correção do modo de debug para a lista de tokens no analisador

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
             text = input('(calc) >> ')
         except EOFError:
             break
-        if text:
-            token_list = lexer.tokenize(text)
-            if debug_token_list:            
-                for token in token_list:
-                    print('type=%r, value=%r' % (token.type, token.value))
-            parser.parse(token_list)
+        if text:            
+            if debug_token_list is True:
+                for token in lexer.tokenize(text):
+                    print('type=%r, value=%r' % (token.type, token.value))            
+                
+            parser.parse(lexer.tokenize(text))        
